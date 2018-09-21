10183414f36cf455b0b64c4dd182dece90e92497
ksyhds <ksyhds@Minearena>
2018-09-21 19:29:10 +0900

    NID1.13.1

--- NameItemDrop 1.13.1/src/com/gmail/Moon_Eclipse/nid/NameItemDrop.java
+++ NameItemDrop 1.13.1/src/com/gmail/Moon_Eclipse/nid/NameItemDrop.java
@@ -1,36 +1,26 @@
 package com.gmail.Moon_Eclipse.nid;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Color;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Creature;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Slime;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.gmail.Moon_Eclipse.MCgive.*;
 import com.gmail.Moon_Eclipse.MCgive.ItemDeliver.ItemDeliver;
 import com.Moon_eclipse.EclipseLib.LibMain;
 import com.Moon_eclipse.EclipseLib.ItemCreater.ItemCreator;
